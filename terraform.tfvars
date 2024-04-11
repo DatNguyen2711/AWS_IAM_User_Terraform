e8f314f0a56d347e19f20c865c8b1e03bc360ceb
DatLaid <datlaid@gmail.com>
2024-04-12 02:49:45 +0700

    create success

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+username    = "datlaid"
+policy_name = "ec2_full_access_policy"
+access_key  = "AKIA6GBMG5EXYVHDZXU2"
+secret_key  = "8gjmZjccl2G4mhYQRXjM2f0MaSv2/DSjvI24YpeK"
---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "username" {
+  type    = string
+  default = "example"
+}
+
+
+variable "policy_name" {
+  type    = string
+  default = "default_policy_name"
+}
+
+
+variable "access_key" {
+  type = string
+
+}
+variable "secret_key" {
+  type = string
+
+}
